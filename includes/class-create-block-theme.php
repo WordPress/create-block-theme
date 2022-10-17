570c2aea5ae41103dee0203583abc74f3e5a34c6
Matias Benedetto <matias.benedetto@gmail.com>
2022-10-18 00:08:32 +0100

    renaming php file to reflect the current functionality

--- includes/class-create-block-theme.php
+++ includes/class-create-block-theme.php
@@ -40,7 +40,7 @@ class Create_Block_Theme {
 		 * The class responsible for defining all actions that occur in the admin area.
 		 */
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-create-block-theme-admin.php';
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-embed-fonts-in-theme.php';
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-manage-fonts.php';
 
 		$this->loader = new Create_Block_Theme_Loader();
 
