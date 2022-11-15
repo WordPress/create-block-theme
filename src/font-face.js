fe722b1cd8bfd1b066e520565beb52cbf6bb4aff
Jeff Ong <jonger4@gmail.com>
2022-11-15 11:26:04 -0500

    Allow previewing font family with no font faces.

--- src/font-face.js
+++ src/font-face.js
@@ -2,23 +2,21 @@ import { Button } from '@wordpress/components';
 
 const { __ } = wp.i18n;
 
-function FontFace ( { fontFace, demoText, deleteFontFace } ) {
+function FontFace ( { fontFamily, fontWeight, fontStyle, demoText, deleteFontFace } ) {
 
     // Handle cases like fontWeight is a number instead of a string or when the fontweight is a 'range', a string like "800 900".
-    const fontWeight = fontFace.fontWeight ? String(fontFace.fontWeight).split(' ')[0] : "normal";
-
     const demoStyles = {
-        fontFamily: fontFace.fontFamily,
-        fontStyle: fontFace.fontStyle,
-        fontWeight: fontWeight,
+        fontFamily,
+        fontStyle,
+        fontWeight: fontWeight ? String(fontWeight).split(' ')[0] : "normal",
     };
 
     return (
         <tr className="font-face">
-            <td>{fontFace.fontStyle}</td>
-            <td>{fontFace.fontWeight}</td>
+            <td>{fontStyle}</td>
+            <td>{fontWeight}</td>
             <td className="demo-cell"><p style={ demoStyles }>{demoText}</p></td>
-            <td><Button variant="tertiary" isDestructive={true} onClick={deleteFontFace}>{__('Remove')}</Button></td>
+            { deleteFontFace && <td><Button variant="tertiary" isDestructive={true} onClick={deleteFontFace}>{__('Remove')}</Button></td> }
         </tr>
     );
 }
