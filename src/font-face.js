94f45d3a96aeb4ec1d97cfbc324e2eccdc59c0e9
Sarah Norris <sarah@sekai.co.uk>
2023-01-27 17:05:52 +0000

    Results of lint:js:fix

--- src/font-face.js
+++ src/font-face.js
@@ -1,41 +1,55 @@
-import { Button } from '@wordpress/components'; 
+import { Button } from '@wordpress/components';
 
 const { __ } = wp.i18n;
 
-function FontFace ( {
-    fontFamily,
-    fontWeight,
-    fontStyle,
-    demoText,
-    deleteFontFace,
-    shouldBeRemoved,
-} ) {
-    
-    const demoStyles = {
-        fontFamily,
-        fontStyle,
-        // Handle cases like fontWeight is a number instead of a string or when the fontweight is a 'range', a string like "800 900".
-        fontWeight: fontWeight ? String(fontWeight).split(' ')[0] : "normal",
-    };
+function FontFace({
+	fontFamily,
+	fontWeight,
+	fontStyle,
+	demoText,
+	deleteFontFace,
+	shouldBeRemoved,
+}) {
+	const demoStyles = {
+		fontFamily,
+		fontStyle,
+		// Handle cases like fontWeight is a number instead of a string or when the fontweight is a 'range', a string like "800 900".
+		fontWeight: fontWeight ? String(fontWeight).split(' ')[0] : 'normal',
+	};
 
-    if ( shouldBeRemoved ) {
-        return null;
-    }
+	if (shouldBeRemoved) {
+		return null;
+	}
 
-    return (
-        <tr className="font-face">
-            <td>{fontStyle}</td>
-            <td>{fontWeight}</td>
-            <td className="demo-cell"><p style={ demoStyles }>{demoText}</p></td>
-            { deleteFontFace && <td><Button variant="tertiary" isDestructive={true} onClick={deleteFontFace}>{__('Remove', 'create-block-theme')}</Button></td> }
-        </tr>
-    );
+	return (
+		<tr className="font-face">
+			<td>{fontStyle}</td>
+			<td>{fontWeight}</td>
+			<td className="demo-cell">
+				<p style={demoStyles}>{demoText}</p>
+			</td>
+			{deleteFontFace && (
+				<td>
+					<Button
+						variant="tertiary"
+						isDestructive={true}
+						onClick={deleteFontFace}
+					>
+						{__('Remove', 'create-block-theme')}
+					</Button>
+				</td>
+			)}
+		</tr>
+	);
 }
 
 FontFace.defaultProps = {
-    demoText: __("The quick brown fox jumps over the lazy dog.", "create-block-theme"),
-    fontWeight: "normal",
-    fontStyle: "normal",
+	demoText: __(
+		'The quick brown fox jumps over the lazy dog.',
+		'create-block-theme'
+	),
+	fontWeight: 'normal',
+	fontStyle: 'normal',
 };
 
 export default FontFace;
