b2389067f49f2fa613b3b50fa322a0d24fa15529
Sarah Norris <sarah@sekai.co.uk>
2023-01-30 18:52:13 +0000

    Fix PHP lint errors

--- includes/class-create-block-theme.php
+++ includes/class-create-block-theme.php
@@ -55,7 +55,7 @@ class Create_Block_Theme {
 	 */
 	private function define_admin_hooks() {
 
-		$plugin_admin = new Create_Block_Theme_Admin();
+		$plugin_admin       = new Create_Block_Theme_Admin();
 		$manage_fonts_admin = new Manage_Fonts_Admin();
 	}
 
