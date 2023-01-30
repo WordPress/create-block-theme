074918f32be07f0d275021a138c151d80080efee
Sarah Norris <sarah@sekai.co.uk>
2023-01-30 19:01:04 +0000

    Fix js lint errors

--- admin/js/form-script.js
+++ admin/js/form-script.js
@@ -1,8 +1,8 @@
 function toggleForm(formID, hide) {
-	if (formID == 'new_theme_metadata_form' && !hide) {
+	if (formID === 'new_theme_metadata_form' && !hide) {
 		toggleForm('new_variation_metadata_form', true);
 	}
-	if (formID == 'new_variation_metadata_form' && !hide) {
+	if (formID === 'new_variation_metadata_form' && !hide) {
 		toggleForm('new_theme_metadata_form', true);
 	}
 	document.getElementById(formID).toggleAttribute('hidden', hide);
