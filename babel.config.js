b8d66b7abddce7d603b17cb7821f1c6c95c09140
Sarah Norris <sarah@sekai.co.uk>
2023-02-01 10:39:42 +0000

    Update babel settings

--- babel.config.js
+++ babel.config.js
@@ -4,13 +4,5 @@ module.exports = (api) => {
 	return {
 		presets: ['@wordpress/babel-preset-default'],
 		plugins: ['@emotion/babel-plugin', 'babel-plugin-inline-json-import'],
-		overrides: [
-			{
-				test: 'packages/block-library/src/index.js',
-				plugins: [
-					require.resolve('@wordpress/block-library/babel-plugin'),
-				],
-			},
-		],
 	};
 };
