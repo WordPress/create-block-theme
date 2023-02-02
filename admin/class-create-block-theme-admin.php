e730c909981f64ede1081a627ef42b75c4d9fd73
Matias Benedetto <matias.benedetto@gmail.com>
2023-02-02 14:35:19 +0800

    removing not needed code formatting

--- admin/class-create-block-theme-admin.php
+++ admin/class-create-block-theme-admin.php
@@ -240,7 +240,7 @@ class Create_Block_Theme_Admin {
 			'style.css',
 			$this->build_child_style_css( $theme )
 		);
-
+		
 		// Add / replace screenshot.
 		if ( $this->is_valid_screenshot( $screenshot ) ){
 			$zip->addFile(
@@ -299,8 +299,8 @@ class Create_Block_Theme_Admin {
 		if ( ! file_exists( $blank_theme_path ) ) {
 			mkdir( $blank_theme_path, 0755 );
 			// Add readme.txt.
-			file_put_contents(
-				$blank_theme_path . DIRECTORY_SEPARATOR . 'readme.txt',
+			file_put_contents( 
+				$blank_theme_path . DIRECTORY_SEPARATOR . 'readme.txt', 
 				$this->build_readme_txt( $theme )
 			);
 
@@ -316,8 +316,8 @@ class Create_Block_Theme_Admin {
 			}
 
 			// Add style.css.
-			file_put_contents(
-				$blank_theme_path . DIRECTORY_SEPARATOR . 'style.css',
+			file_put_contents( 
+				$blank_theme_path . DIRECTORY_SEPARATOR . 'style.css', 
 				$css_contents
 			);
 
@@ -369,7 +369,7 @@ class Create_Block_Theme_Admin {
 		if ( ! file_exists( $variation_path ) ) {
 			mkdir( $variation_path, 0755, true );
 		}
-
+		
 		if ( file_exists( $variation_path . $variation_slug . '.json' ) ) {
 			$file_counter++;
 			while ( file_exists( $variation_path . $variation_slug . '_' . $file_counter . '.json' ) ) {
@@ -379,7 +379,7 @@ class Create_Block_Theme_Admin {
 		}
 
 		$_POST['theme']['variation_slug'] = $variation_slug;
-
+		
 		file_put_contents(
 			$variation_path . $variation_slug . '.json',
 			MY_Theme_JSON_Resolver::export_theme_data( $export_type )
