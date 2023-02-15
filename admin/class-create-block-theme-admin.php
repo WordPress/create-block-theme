c59c50e07db3c2e4b6909a4a1c2f35958ba3b12b
Matias Benedetto <matias.benedetto@gmail.com>
2023-02-15 18:00:37 +0700

    Implementing WP_HTML_Tag_Processor to get and set element html attributes and fallback to DOMDocument and regex if WP_HTML_Tag_Processor is not available yet

--- admin/class-create-block-theme-admin.php
+++ admin/class-create-block-theme-admin.php
@@ -568,32 +568,64 @@ class Create_Block_Theme_Admin {
 		if ( empty( $html ) ) {
 			return $html;
 		}
-		$doc = new DOMDocument();
-		@$doc->loadHTML( $html, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD );
-		$img_tags = $doc->getElementsByTagName( 'img' );
-		// replace all images that have absolute urls
-		foreach ( $img_tags as $tag ) {
-			$image_url = $tag->getAttribute( 'src' );
-			if ( $this->is_absolute_url( $image_url ) ) {
-				$img_src = $tag->getAttribute( 'src' );
-				$html = str_replace( $img_src, $this->make_relative_image_url( $img_src ), $html );
+
+		// If the WP_HTML_Tag_Processor class exists, use it to parse the HTML.
+		// This API was recently in Gutenberg and not yet available in WordPress core. https://github.com/WordPress/gutenberg/pull/42485
+		// If it's not available, fallb ack to DOMDocument which can not be installed in all the systems and has some issues.
+		// When WP_HTML_Tag_Processor is availabe in core (6.2) we can remove the DOMDocument fallback.
+		if ( class_exists( 'WP_HTML_Tag_Processor' ) ) {
+			$html = new WP_HTML_Tag_Processor( $html );
+			while ( $html->next_tag( 'img' ) ) {
+				if ( $this->is_absolute_url( $html->get_attribute( 'src' ) ) ) {
+					$html->set_attribute( 'src', $this->make_relative_image_url( $html->get_attribute( 'src' ) ) );
+				}
 			}
-		}
-		// also replace background images with absolute urls (used in cover blocks)
-		$div_tags = $doc->getElementsByTagName( 'div' );
-		foreach ( $div_tags as $tag ) {
-			$style = $tag->getAttribute( 'style' );
-			if ( $style ) {
-				preg_match_all('#\bhttps?://[^,\s()<>]+(?:\([\w\d]+\)|([^,[:punct:]\s]|/))#', $style, $match);
-				$urls = $match[0];
-				foreach ( $urls as $url ) {
-					if ( $this->is_absolute_url( $url ) ) {
-						$html = str_replace( $url, $this->make_relative_image_url( $url ), $html );
+			while ( $html->next_tag( 'div' ) ) {
+				$style = $html->get_attribute( 'style' );
+				if ( $style ) {
+					preg_match_all('#\bhttps?://[^,\s()<>]+(?:\([\w\d]+\)|([^,[:punct:]\s]|/))#', $style, $match);
+					$urls = $match[0];
+					foreach ( $urls as $url ) {
+						if ( $this->is_absolute_url( $url ) ) {
+							$html->set_attribute( 'style', str_replace( $url, $this->make_relative_image_url( $url ), $style ) );
+						}
 					}
 				}
 			}
+			return $html->__toString();
 		}
-		return $html;
+		
+		// Fallback to DOMDocument.
+		// TODO: When WP_HTML_Tag_Processor is availabe in core (6.2) we can remove this implementation entirely.
+		if ( ! class_exists( 'WP_HTML_Tag_Processor' ) ) { 
+			$doc = new DOMDocument();
+			@$doc->loadHTML( $html, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD );
+			$img_tags = $doc->getElementsByTagName( 'img' );
+			// replace all images that have absolute urls
+			foreach ( $img_tags as $tag ) {
+				$image_url = $tag->getAttribute( 'src' );
+				if ( $this->is_absolute_url( $image_url ) ) {
+					$img_src = $tag->getAttribute( 'src' );
+					$html = str_replace( $img_src, $this->make_relative_image_url( $img_src ), $html );
+				}
+			}
+			// also replace background images with absolute urls (used in cover blocks)
+			$div_tags = $doc->getElementsByTagName( 'div' );
+			foreach ( $div_tags as $tag ) {
+				$style = $tag->getAttribute( 'style' );
+				if ( $style ) {
+					preg_match_all('#\bhttps?://[^,\s()<>]+(?:\([\w\d]+\)|([^,[:punct:]\s]|/))#', $style, $match);
+					$urls = $match[0];
+					foreach ( $urls as $url ) {
+						if ( $this->is_absolute_url( $url ) ) {
+							$html = str_replace( $url, $this->make_relative_image_url( $url ), $html );
+						}
+					}
+				}
+			}
+			return $html;
+		}
+
 	}
 
 	function make_image_block_local ( $block ) {
@@ -650,13 +682,13 @@ class Create_Block_Theme_Admin {
 			$inner_blocks = $block['innerBlocks'];
 			switch ( $block[ 'blockName' ] ) {
 				case 'core/image':
-					$block = $this->make_image_block_local($block);
+					$block = $this->make_image_block_local( $block );
 					break;
 				case 'core/cover':
-					$block = $this->make_cover_block_local($block);
+					$block = $this->make_cover_block_local( $block );
 					break;
 				case 'core/media-text':
-					$block = $this->make_mediatext_block_local($block);
+					$block = $this->make_mediatext_block_local( $block );
 					break;
 			}
 			// recursive call for inner blocks
@@ -670,36 +702,87 @@ class Create_Block_Theme_Admin {
 
 	function get_media_absolute_urls_from_blocks ( $flatten_blocks ) {
 		$media = [];
-		foreach ( $flatten_blocks as $block ) {
-			if ( 'core/image' === $block[ 'blockName' ] || 'core/cover' === $block[ 'blockName' ] ) {
-				$doc = new DOMDocument();
-				@$doc->loadHTML( $block['innerHTML'], LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD );
 
-				// Get the media urls from img tags
-				$tags = $doc->getElementsByTagName( 'img' );
-				$block_has_external_images = false;
-				foreach ($tags as $tag) {
-					$image_url = $tag->getAttribute( 'src' );
-					if ($this->is_absolute_url( $image_url )) {
-						$media[] = $tag->getAttribute( 'src' );
+		// If WP_HTML_Tag_Processor is available, use it to get the absolute URLs of img and background images
+		// This class is available in core yet, but it will be available in the future (6.2)
+		// see https://github.com/WordPress/gutenberg/pull/42485
+		if ( class_exists( 'WP_HTML_Tag_Processor' ) ) {
+			foreach ( $flatten_blocks as $block ) {
+				// Gets the absolute URLs of img in these blocks
+				if ( 
+					'core/image' === $block[ 'blockName' ] ||
+					'core/cover' === $block[ 'blockName' ] ||
+					'core/media-text' === $block[ 'blockName' ]
+				) {
+					$html = new WP_HTML_Tag_Processor( $block[ 'innerHTML' ] );
+					while ( $html->next_tag( 'img' ) ) {
+						$url = $html->get_attribute( 'src' );
+						if ( $this->is_absolute_url( $url ) ) {
+							$media[] = $url;
+						}
 					}
 				}
-				// Get the media urls from div style tags (used in cover blocks)
-				$div_tags = $doc->getElementsByTagName( 'div' );
-				foreach ( $div_tags as $tag ) {
-					$style = $tag->getAttribute( 'style' );
-					if ( $style ) {
-						preg_match_all('#\bhttps?://[^,\s()<>]+(?:\([\w\d]+\)|([^,[:punct:]\s]|/))#', $style, $match);
-						$urls = $match[0];
-						foreach ( $urls as $url ) {
-							if ( $this->is_absolute_url( $url ) ) {
-								$media[] = $url;
+
+				// Gets the absolute URLs of background images in these blocks
+				if ( 'core/cover' === $block['blockName'] ) {
+					$html = new WP_HTML_Tag_Processor( $block[ 'innerHTML' ] );
+					while ( $html->next_tag( 'div' ) ) {
+						$style = $html->get_attribute( 'style' );
+						if ( $style ) {
+							$matches = [];
+							preg_match( '/background-image: url\((.*)\)/', $style, $matches );
+							if ( isset( $matches[1] ) ) {
+								$url = $matches[1];
+								if ( $this->is_absolute_url( $url ) ) {
+									$media[] = $url;
+								}
+							}
+						}
+					}
+				}
+
+			}
+		}
+
+		// Fallback to DOMDocument.
+		// TODO: When WP_HTML_Tag_Processor is availabe in core (6.2) we can remove this implementation entirely.
+		if ( ! class_exists ( 'WP_HTML_Tag_Processor' ) ) {
+			foreach ( $flatten_blocks as $block ) {
+				if ( 
+						'core/image' === $block[ 'blockName' ] ||
+						'core/cover' === $block[ 'blockName' ] ||
+						'core/media-text' === $block[ 'blockName' ]
+					) {
+					$doc = new DOMDocument();
+					@$doc->loadHTML( $block['innerHTML'], LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD );
+
+					// Get the media urls from img tags
+					$tags = $doc->getElementsByTagName( 'img' );
+					$block_has_external_images = false;
+					foreach ($tags as $tag) {
+						$image_url = $tag->getAttribute( 'src' );
+						if ($this->is_absolute_url( $image_url )) {
+							$media[] = $tag->getAttribute( 'src' );
+						}
+					}
+					// Get the media urls from div style tags (used in cover blocks)
+					$div_tags = $doc->getElementsByTagName( 'div' );
+					foreach ( $div_tags as $tag ) {
+						$style = $tag->getAttribute( 'style' );
+						if ( $style ) {
+							preg_match_all('#\bhttps?://[^,\s()<>]+(?:\([\w\d]+\)|([^,[:punct:]\s]|/))#', $style, $match);
+							$urls = $match[0];
+							foreach ( $urls as $url ) {
+								if ( $this->is_absolute_url( $url ) ) {
+									$media[] = $url;
+								}
 							}
 						}
 					}
 				}
 			}
 		}
+
 		return $media;
 	}
 
@@ -753,17 +836,38 @@ class Create_Block_Theme_Admin {
 	}
 
 	function escape_alt_for_pattern ( $html ) {
-		preg_match( '@alt="([^"]+)"@' , $html, $match );
-		if ( isset( $match[0] ) ) {
-			$alt_attribute = $match[0];
-			$alt_value= $match[1];
-			$html = str_replace(
-				$alt_attribute,
-				'alt="'.$this->escape_text_for_pattern( $alt_value ).'"',
-				$html
-			);
+		if ( empty ( $html ) ){
+			return $html;
 		}
-		return $html;
+
+		// Use WP_HTML_Tag_Processor if available
+		// see: https://github.com/WordPress/gutenberg/pull/42485
+		if ( class_exists( 'WP_HTML_Tag_Processor' ) ) {
+			$html = new WP_HTML_Tag_Processor( $html );
+			while ( $html->next_tag( 'img' ) ) {
+				// $alt_attribute = $html->get_attribute( 'alt' );
+				// if ( !empty ( $alt_attribute ) ) {
+					$html->set_attribute( 'alt', 'hola' );
+				// }
+			}
+			return $html->__toString();
+		}
+		
+		// Fallback to regex
+		// TODO: When WP_HTML_Tag_Processor is availabe in core (6.2) we can remove this implementation entirely.
+		if ( ! class_exists( 'WP_HTML_Tag_Processor' ) ) {
+			preg_match( '@alt="([^"]+)"@' , $html, $match );
+			if ( isset( $match[0] ) ) {
+				$alt_attribute = $match[0];
+				$alt_value= $match[1];
+				$html = str_replace(
+					$alt_attribute,
+					'alt="'.$this->escape_text_for_pattern( $alt_value ).'"',
+					$html
+				);
+			}
+			return $html;
+		}
 	}
 
 	/**
