2885cf7026d969bb55cff69f37282cca54d6abca
madhusudhand <madhusudhan.dollu@gmail.com>
2023-04-13 18:27:19 +0530

    refactor react app bootstrapping code

--- admin/manage-fonts/google-fonts-page.php
+++ admin/manage-fonts/google-fonts-page.php
@@ -1,10 +1,10 @@
 <?php
 
-require_once( __DIR__ . '/fonts-page.php' );
+require_once( dirname( __DIR__ ) . '/class-react-app.php' );
 
 class Google_Fonts {
 	public static function google_fonts_admin_page() {
-		Fonts_Page::load_fonts_react_app();
+		React_App::bootstrap();
 		?>
 		<input id="nonce" type="hidden" value="<?php echo wp_create_nonce( 'create_block_theme' ); ?>" />
 		<div id="fonts-app"></div>
