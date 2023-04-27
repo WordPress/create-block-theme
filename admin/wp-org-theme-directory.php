5f7b5dc61c4c0fb52e829cbec48af29fb76d8319
Sarah Norris <sarah@sekai.co.uk>
2023-04-27 15:43:08 +0100

    Fix PHP lint errors

--- admin/wp-org-theme-directory.php
+++ admin/wp-org-theme-directory.php
@@ -2,48 +2,51 @@
 
 class WP_Theme_Directory {
 
-    const THEME_NAMES_ENDPOINT = 'https://themes.svn.wordpress.org/';
+	const THEME_NAMES_ENDPOINT = 'https://themes.svn.wordpress.org/';
 
-    /**
+	/**
 	 * Initialize the class and set its properties.
 	 */
 	public function __construct() {
 		add_action( 'rest_api_init', array( $this, 'register_theme_names_endpoint' ) );
-        add_action( 'admin_init', array( $this, 'assets_enqueue' ) );
+		add_action( 'admin_init', array( $this, 'assets_enqueue' ) );
 	}
 
-    public static function register_theme_names_endpoint () {
-        register_rest_route(
-            'create-block-theme/v1',
-            '/wp-org-theme-names',
-            array(
-                'methods' => 'GET',
-                'callback' => array( 'WP_Theme_Directory', 'get_theme_names' ),
-                'permission_callback' => function () {
-                    return current_user_can( 'edit_theme_options' );
-                },
-            )
-        );
-        
-    }
+	public static function register_theme_names_endpoint() {
+		register_rest_route(
+			'create-block-theme/v1',
+			'/wp-org-theme-names',
+			array(
+				'methods'             => 'GET',
+				'callback'            => array( 'WP_Theme_Directory', 'get_theme_names' ),
+				'permission_callback' => function () {
+					return current_user_can( 'edit_theme_options' );
+				},
+			)
+		);
 
-    public static function get_theme_names () {
-        $html = wp_remote_get( self::THEME_NAMES_ENDPOINT );
+	}
 
-        // parse the html response extracting all the a inside li elements
-        $pattern = '/<li><a href=".*?">(.*?)<\/a><\/li>/';
-        preg_match_all($pattern, $html['body'], $matches);
-        
-        // Revemo the / from the end of the theme name
-        $cleaned_names = array_map( function ( $name ) {
-            return str_replace( '/', '', $name );
-        }, $matches[1] );
+	public static function get_theme_names() {
+		$html = wp_remote_get( self::THEME_NAMES_ENDPOINT );
 
-        $names = array( 'names' => $cleaned_names );
-        return rest_ensure_response( $names );
-    }
-    
-    function assets_enqueue() {
+		// parse the html response extracting all the a inside li elements
+		$pattern = '/<li><a href=".*?">(.*?)<\/a><\/li>/';
+		preg_match_all( $pattern, $html['body'], $matches );
+
+		// Revemo the / from the end of the theme name
+		$cleaned_names = array_map(
+			function ( $name ) {
+				return str_replace( '/', '', $name );
+			},
+			$matches[1]
+		);
+
+		$names = array( 'names' => $cleaned_names );
+		return rest_ensure_response( $names );
+	}
+
+	function assets_enqueue() {
 		$asset_file = include( plugin_dir_path( dirname( __FILE__ ) ) . 'build/wp-org-theme-directory.asset.php' );
 
 		wp_register_script(
@@ -57,8 +60,8 @@ class WP_Theme_Directory {
 			'wp-org-theme-directory',
 		);
 
-        // Initialize and empty array of theme names to be shared between different client side scripts
-        wp_localize_script(
+		// Initialize and empty array of theme names to be shared between different client side scripts
+		wp_localize_script(
 			'wp-org-theme-directory',
 			'wpOrgThemeDirectory',
 			array(
@@ -69,4 +72,4 @@ class WP_Theme_Directory {
 
 }
 
-?>
+
