f4457595b6a5d2eb4ca52f320f577999fd225702
Grant Kinney <hi@grant.mk>
2023-11-01 13:31:43 -0500

    Updates theme form markup

--- src/wp-org-theme-directory.js
+++ src/wp-org-theme-directory.js
@@ -4,8 +4,14 @@ async function loadUnavailableThemeNames() {
 	const requestOptions = {
 		path: '/create-block-theme/v1/wp-org-theme-names',
 	};
-	const request = await apiFetch( requestOptions );
-	wpOrgThemeDirectory.themeSlugs = request.names;
+
+	try {
+		const request = await apiFetch( requestOptions );
+		window.wpOrgThemeDirectory.themeSlugs = request.names;
+	} catch ( error ) {
+		// eslint-disable-next-line no-console
+		console.error( error );
+	}
 }
 
 window.addEventListener( 'load', loadUnavailableThemeNames );
