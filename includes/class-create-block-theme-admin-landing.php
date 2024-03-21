65f6231a8d89668381a19bf2acd2aaecfe20696a
Jason Crist <jcrist@pbking.com>
2024-03-21 11:56:09 -0400

    Got Landing Page showing

--- includes/class-create-block-theme-admin-landing.php
+++ includes/class-create-block-theme-admin-landing.php
@@ -12,32 +12,14 @@ class Create_Block_Theme_Admin_Landing {
 		}
 
 		$landing_page_slug     = 'create-block-theme-landing';
-		$landing_page_title = _x( 'Manage Theme Fonts', 'UI String', 'create-block-theme' );
+		$landing_page_title = _x( 'Create Block Theme', 'UI String', 'create-block-theme' );
 		$landing_page_menu_title = $landing_page_title;
-		add_theme_page( $landing_page_title, $landing_page_menu_title, 'edit_theme_options', $landing_page_slug, array( 'Landing_Page', $landing_page_slug ) );
+		add_theme_page( $landing_page_title, $landing_page_menu_title, 'edit_theme_options', $landing_page_slug, array( 'Create_Block_Theme_Admin_Landing', 'admin_menu_page' ) );
 
-		// Check if the admin page title is set, and if not, set one.
-		// This is needed to avoid a warning in the admin menu, due to the admin page title not being set in
-		// the add_submenu_page() function for the Google Fonts and Local Fonts pages.
-		global $title;
+	}
 
-		// Check if current admin page is a create block theme page.
-		// Context: https://github.com/WordPress/create-block-theme/issues/478
-		$admin_page        = isset( $_GET['page'] ) ? sanitize_text_field( wp_unslash( $_GET['page'] ) ) : '';
-		$manage_font_pages = array(
-			$manage_font_page_slug,
-			$add_google_font_page_slug,
-			$add_local_font_page_slug,
-		);
-
-		$is_manage_fonts_page = in_array( $admin_page, $manage_font_pages, true );
-
-		if ( ! isset( $title ) && $is_manage_fonts_page ) {
-			$title = $manage_fonts_page_title;
-		}
-
-		$local_fonts_page_title = _x( 'Embed local font in the active theme', 'UI String', 'create-block-theme' );
-		$local_fonts_menu_title = $local_fonts_page_title;
-		add_submenu_page( '', $local_fonts_page_title, $local_fonts_menu_title, 'edit_theme_options', $add_local_font_page_slug, array( 'Local_Fonts', 'local_fonts_admin_page' ) );
+	public static function admin_menu_page() {
+		echo '<div id="create-block-theme-app">Landing Page to go Here</div>';
 	}
 }
+
