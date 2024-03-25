649ba9d952b9d83ceed5e41261951ee2051ef6cd
Jason Crist <jcrist@pbking.com>
2024-03-25 14:49:00 -0400

    Added tests that evaluate encoded patterns and test patterns created from templates with media

--- tests/test-theme-media.php
+++ tests/test-theme-media.php
@@ -39,7 +39,25 @@ class Test_Create_Block_Theme_Media extends WP_UnitTestCase {
 		$this->assertStringContainsString( '/assets/images', $new_template->content );
 	}
 
-	// TODO: Test additional blocks
-	// TODO: Test nested blocks
+	public function test_template_with_media_correctly_prepared() {
+		$template          = new stdClass();
+		$template->slug    = 'test-template';
+		$template->content = '
+			<!-- wp:image -->
+			<figure class="wp-block-image"><img src="http://example.com/image.jpg" alt="Alternative Text" /></figure>
+			<!-- /wp:image -->
+		';
+		$new_template      = Theme_Templates::prepare_template_for_export( $template );
+
+		// Content should be replaced with a pattern block
+		$this->assertStringContainsString( '<!-- wp:pattern', $new_template->content );
+
+		// The media to install should be in the collection
+		$this->assertContains( 'http://example.com/image.jpg', $new_template->media );
+
+		// The pattern is correctly encoded
+		$this->assertStringContainsString( '<img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/image.jpg"', $new_template->pattern );
+
+	}
 
 }
