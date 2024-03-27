05b48f10123a7db14cedd71a416c0128bdfc5055
Vicente Canales <vicente.canales@automattic.com>
2024-03-27 08:43:37 -0300

    add theme name to editor modal title

--- src/editor-sidebar/json-editor-modal.js
+++ src/editor-sidebar/json-editor-modal.js
@@ -1,15 +1,19 @@
 import { useState, useEffect } from '@wordpress/element';
 import { Modal } from '@wordpress/components';
+import { useSelect } from '@wordpress/data';
 import CodeMirror from '@uiw/react-codemirror';
 import { json } from '@codemirror/lang-json';
 import { fetchThemeJson } from '../resolvers';
 
 const ThemeJsonEditorModal = ( { onRequestClose } ) => {
 	const [ themeData, setThemeData ] = useState( '' );
-
+	const themeName = useSelect( ( select ) =>
+		select( 'core' ).getCurrentTheme()
+	)?.name?.raw;
 	const fetchThemeData = async () => {
 		setThemeData( await fetchThemeJson() );
 	};
+	const handleSave = () => {};
 
 	useEffect( () => {
 		fetchThemeData();
@@ -18,13 +22,13 @@ const ThemeJsonEditorModal = ( { onRequestClose } ) => {
 	return (
 		<Modal
 			isFullScreen
-			title={ <>theme.json</> }
+			title={ `theme.json for ${ themeName }` }
 			onRequestClose={ onRequestClose }
 		>
 			<CodeMirror
 				extensions={ [ json() ] }
 				value={ themeData }
-				// onChange={ onChange }
+				onChange={ handleSave }
 			/>
 		</Modal>
 	);
