294dd70080d44e5dc1879e8730abaf51c650f96c
Jason Crist <jcrist@pbking.com>
2024-04-03 08:12:59 -0400

    Added creation of theme validation to site editor interface (#532)

--- admin/create-theme/theme-json.php
+++ admin/create-theme/theme-json.php
@@ -10,33 +10,27 @@ class Theme_Json {
 	}
 
 	public static function add_theme_json_variation_to_local( $export_type, $theme ) {
-		$variation_slug = sanitize_title( $theme['variation'] );
 		$variation_path = get_stylesheet_directory() . DIRECTORY_SEPARATOR . 'styles' . DIRECTORY_SEPARATOR;
-		$file_counter   = 0;
 
 		if ( ! file_exists( $variation_path ) ) {
 			wp_mkdir_p( $variation_path );
 		}
 
-		if ( file_exists( $variation_path . $variation_slug . '.json' ) ) {
-			$file_counter++;
-			while ( file_exists( $variation_path . $variation_slug . '_' . $file_counter . '.json' ) ) {
-				$file_counter++;
-			}
-			$variation_slug = $variation_slug . '_' . $file_counter;
+		if ( file_exists( $variation_path . $theme['slug'] . '.json' ) ) {
+			return new WP_Error( 'variation_already_exists', __( 'Variation already exists.', 'create-block-theme' ) );
 		}
 
-		$_POST['theme']['variation_slug'] = $variation_slug;
+		$_POST['theme']['variation_slug'] = $theme['slug'];
 
 		$extra_theme_data = array(
 			'version' => WP_Theme_JSON::LATEST_SCHEMA,
-			'title'   => $theme['variation'],
+			'title'   => $theme['name'],
 		);
 
 		$variation_theme_json = MY_Theme_JSON_Resolver::export_theme_data( $export_type, $extra_theme_data );
 
 		file_put_contents(
-			$variation_path . $variation_slug . '.json',
+			$variation_path . $theme['slug'] . '.json',
 			$variation_theme_json
 		);
 	}
