0b5d20211fff56f900d3c7589af428c6118d3815
Jason Crist <jcrist@pbking.com>
2024-04-25 14:02:14 -0400

    removed unused utility functions and their unit tests (leaving a sample unit test in its place rather than dismantling the unit tests

--- src/utils.js
+++ src/utils.js
@@ -1,24 +1,3 @@
-import { __ } from '@wordpress/i18n';
-
-export function forceHttps( url ) {
-	return url.replace( 'http://', 'https://' );
-}
-
-export function bytesToSize( bytes ) {
-	const sizes = [ 'Bytes', 'KB', 'MB', 'GB', 'TB' ];
-	if ( bytes === 0 ) return __( 'n/a', 'create-block-theme' );
-	const i = parseInt( Math.floor( Math.log( bytes ) / Math.log( 1024 ) ) );
-	if ( i === 0 ) return bytes + ' ' + sizes[ i ];
-	return ( bytes / Math.pow( 1024, i ) ).toFixed( 1 ) + ' ' + sizes[ i ];
-}
-
-export function localFileAsThemeAssetUrl( url ) {
-	if ( ! url ) {
-		return url;
-	}
-	return url.replace( 'file:./', window.createBlockTheme.themeUrl + '/' );
-}
-
 export async function downloadFile( response ) {
 	const blob = await response.blob();
 	const filename = response.headers
@@ -45,14 +24,4 @@ export async function downloadFile( response ) {
 			URL.revokeObjectURL( url );
 		}, 100 );
 	}
-}
-
-/*
- * Add quotes to font name.
- * @param {string} familyName The font family name.
- * @return {string} The font family name with quotes.
- */
-
-export function addQuotesToName( familyName ) {
-	return `'${ familyName }'`.trim();
 }
