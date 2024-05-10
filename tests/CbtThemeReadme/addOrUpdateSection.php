769cb08e5fc9e230565aa7288bd144a94f5d1917
Matias Benedetto <matias.benedetto@gmail.com>
2024-05-10 11:44:12 -0300

    Add tests for ThemeReadme class public methods

---
+++ tests/CbtThemeReadme/addOrUpdateSection.php
@@ -0,0 +1,50 @@
+<?php
+
+require_once __DIR__ . '/base.php';
+
+/**
+ * Test the add_or_update_section method of the Theme_Readme class.
+ *
+ * @package Create_Block_Theme
+ * @covers Theme_Readme::add_or_update_section
+ * @group readme
+ *
+ */
+class CBT_ThemeReadme_AddOrUpdateSection extends CBT_Theme_Readme_UnitTestCase {
+	public function test_add_or_update_section() {
+		$section_title   = 'Test Section';
+		$section_content = 'Test content abc123';
+
+		// Add a new section.
+		$readme = Theme_Readme::add_or_update_section( $section_title, $section_content );
+
+		// Check if the section was added.
+		$this->assertStringContainsString( $section_title, $readme, 'The section title is missing.' );
+		$this->assertStringContainsString( $section_content, $readme, 'The section content is missing' );
+
+		// Update the section.
+		$section_content_updated = 'Updated content xyz890';
+
+		$readme = Theme_Readme::add_or_update_section( $section_title, $section_content_updated );
+
+		// Check if the old content was updated.
+		$this->assertStringNotContainsString( $section_content, $readme, 'The old content is still present.' );
+
+		// Check if the new content was added.
+		$this->assertStringContainsString( $section_title, $readme, 'The section title is missing.' );
+		$this->assertStringContainsString( $section_content_updated, $readme, 'The updated content is missing.' );
+
+		// Check if that the section title was added only once.
+		$section_count = substr_count( $readme, $section_title );
+		$this->assertEquals( 1, $section_count, 'The section title was added more than once.' );
+	}
+
+	public function test_add_or_update_section_with_no_content() {
+		$section_title   = 'Test Section';
+		$section_content = '';
+
+		// Empty section should not be added.
+		$readme = Theme_Readme::add_or_update_section( $section_title, $section_content );
+		$this->assertStringNotContainsString( $section_title, $readme, 'The title of an empty section should not be added.' );
+	}
+}
---
+++ tests/data/themes/test-theme-readme/readme.txt
@@ -0,0 +1,54 @@
+=== Test Readme Theme ===
+Contributors: wordpressdotorg
+Requires at least: 6.5
+Tested up to: 6.5
+Requires PHP: 7.0
+Stable tag: 2.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+
+== Description ==
+
+Test Readme Theme is a test theme created with the sole purpose of testing the Create_Block_Theme_Readme class.
+
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
+
+= 1.1 =
+* Added new feature
+
+= 2.0 =
+* Added new major feature
+
+= 2.1 =
+* Added new feature
+
+
+== Copyright ==
+
+Test Readme Theme WordPress Theme, (C) 2023 WordPress.org
+Test Readme Theme is distributed under the terms of the GNU GPL.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+
+=== Images ===
+
+License: CC0 https://creativecommons.org/publicdomain/zero/1.0/
+
+museum.webp - https://www.rawpixel.com/image/3297419/free-photo-image-interior-hallway-architecture
+tourist-and-building.webp - https://www.rawpixel.com/image/5928004/photo-image-public-domain-hand-person
+
+
