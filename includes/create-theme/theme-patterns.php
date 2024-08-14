6cf44384ae59b57db9655379c0d2f55f4d1df536
Sarah Norris <sarah@sekai.co.uk>
2024-08-14 15:19:19 +0100

    Refactor PHP content of pattern_from_wp_block
    Co-Authored-By: Elliott Richmond <elliott@squareonemd.co.uk>

--- includes/create-theme/theme-patterns.php
+++ includes/create-theme/theme-patterns.php
@@ -30,18 +30,17 @@ class CBT_Theme_Patterns {
 		$pattern->slug         = wp_get_theme()->get( 'TextDomain' ) . '/' . $pattern->name;
 		$pattern_category_list = get_the_terms( $pattern->id, 'wp_pattern_category' );
 		$pattern->categories   = ! empty( $pattern_category_list ) ? join( ', ', wp_list_pluck( $pattern_category_list, 'name' ) ) : '';
-		// $pattern->sync_status  = get_post_meta( $pattern->id, 'wp_pattern_sync_status', true );
-		$pattern->content = (
-		'<?php
-/**
- * Title: ' . $pattern->title . '
- * Slug: ' . $pattern->slug . '
- * Categories: ' . $pattern->categories . '
- * Inserter: no
- */
-?>
-' . $pattern_post->post_content
-		);
+		$pattern->sync_status  = get_post_meta( $pattern->id, 'wp_pattern_sync_status', true );
+		$pattern->content      = <<<PHP
+		<?php
+		/**
+		 * Title: {$pattern->title}
+		 * Slug: {$pattern->slug}
+		 * Categories: {$pattern->categories}
+		 */
+		?>
+		{$pattern_post->post_content}
+		PHP;
 
 		return $pattern;
 	}
