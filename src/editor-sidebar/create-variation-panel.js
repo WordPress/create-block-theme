99edfd4643ae632d70cbccd06f06f992869982f8
Matias Benedetto <matias.benedetto@gmail.com>
2024-08-30 12:10:51 -0300

    add option in frontend to opt-in saving font assets to local theme folder

--- src/editor-sidebar/create-variation-panel.js
+++ src/editor-sidebar/create-variation-panel.js
@@ -3,24 +3,33 @@
  */
 import { __ } from '@wordpress/i18n';
 import { useState } from '@wordpress/element';
-import { useDispatch } from '@wordpress/data';
+import { useDispatch, useSelect } from '@wordpress/data';
 import { store as noticesStore } from '@wordpress/notices';
 import {
 	// eslint-disable-next-line @wordpress/no-unsafe-wp-apis
 	__experimentalVStack as VStack,
 	// eslint-disable-next-line @wordpress/no-unsafe-wp-apis
 	__experimentalText as Text,
+	// eslint-disable-next-line @wordpress/no-unsafe-wp-apis
+	__experimentalSpacer as Spacer,
+	// eslint-disable-next-line @wordpress/no-unsafe-wp-apis
+	__experimentalView as View,
 	PanelBody,
 	Button,
 	TextControl,
+	CheckboxControl,
 } from '@wordpress/components';
 import { copy } from '@wordpress/icons';
+import { store as preferencesStore } from '@wordpress/preferences';
 
 /**
  * Internal dependencies
  */
 import { postCreateThemeVariation } from '../resolvers';
 import ScreenHeader from './screen-header';
+
+const PREFERENCE_SCOPE = 'create-block-theme';
+const PREFERENCE_KEY = 'create-variation';
 
 export const CreateVariationPanel = () => {
 	const { createErrorNotice } = useDispatch( noticesStore );
@@ -29,8 +38,32 @@ export const CreateVariationPanel = () => {
 		name: '',
 	} );
 
+	const preference = useSelect( ( select ) => {
+		const _preference = select( preferencesStore ).get(
+			PREFERENCE_SCOPE,
+			PREFERENCE_KEY
+		);
+		return {
+			saveFonts: _preference?.saveFonts ?? true,
+		};
+	}, [] );
+
+	const handleTogglePreference = ( key ) => {
+		setPreference( PREFERENCE_SCOPE, PREFERENCE_KEY, {
+			...preference,
+			[ key ]: ! preference[ key ],
+		} );
+	};
+
+	const { set: setPreference } = useDispatch( preferencesStore );
+
 	const handleCreateVariationClick = () => {
-		postCreateThemeVariation( theme.name )
+		const variationPreferences = {
+			name: theme.name,
+			...preference,
+		};
+
+		postCreateThemeVariation( variationPreferences )
 			.then( () => {
 				// eslint-disable-next-line no-alert
 				window.alert(
@@ -57,29 +90,57 @@ export const CreateVariationPanel = () => {
 			<ScreenHeader
 				title={ __( 'Create Variation', 'create-block-theme' ) }
 			/>
+
 			<VStack>
-				<Text>
+				<Text as="p">
 					{ __(
 						'Save the Global Styles changes as a theme variation.',
 						'create-block-theme'
 					) }
 				</Text>
-				<br />
-				<TextControl
-					label={ __( 'Variation name', 'create-block-theme' ) }
-					value={ theme.name }
-					onChange={ ( value ) =>
-						setTheme( { ...theme, name: value } )
-					}
-				/>
-				<br />
-				<Button
-					icon={ copy }
-					variant="primary"
-					onClick={ handleCreateVariationClick }
-				>
-					{ __( 'Create Theme Variation', 'create-block-theme' ) }
-				</Button>
+
+				<View>
+					<Spacer paddingY={ 4 }>
+						<VStack>
+							<TextControl
+								label={ __(
+									'Variation name',
+									'create-block-theme'
+								) }
+								value={ theme.name }
+								onChange={ ( value ) =>
+									setTheme( { ...theme, name: value } )
+								}
+							/>
+
+							<CheckboxControl
+								label={ __(
+									'Save Fonts',
+									'create-block-theme'
+								) }
+								help={ __(
+									'Save active custom fonts to the style variation and copy the assets to the theme folder.',
+									'create-block-theme'
+								) }
+								checked={ preference.saveFonts }
+								onChange={ () =>
+									handleTogglePreference( 'saveFonts' )
+								}
+							/>
+
+							<Button
+								icon={ copy }
+								variant="primary"
+								onClick={ handleCreateVariationClick }
+							>
+								{ __(
+									'Create Theme Variation',
+									'create-block-theme'
+								) }
+							</Button>
+						</VStack>
+					</Spacer>
+				</View>
 			</VStack>
 		</PanelBody>
 	);
