cbc43ac3606a21fae257f49c892130b13268cb4c
Aki Hamano <tetsuaki.hamano@gmail.com>
2025-09-07 17:20:06 +0900

    Restore method call

--- includes/create-theme/theme-styles.php
+++ includes/create-theme/theme-styles.php
@@ -110,10 +110,12 @@ Tags: {$tags}
 	public static function clear_user_styles_customizations() {
 		// Clear all values in the user theme.json
 		$user_custom_post_type_id = WP_Theme_JSON_Resolver::get_user_global_styles_post_id();
+		$global_styles_controller = new WP_REST_Global_Styles_Controller();
 		$update_request           = new WP_REST_Request( 'PUT', '/wp/v2/global-styles/' );
 		$update_request->set_param( 'id', $user_custom_post_type_id );
 		$update_request->set_param( 'settings', array() );
 		$update_request->set_param( 'styles', array() );
+		$global_styles_controller->update_item( $update_request );
 		delete_transient( 'global_styles' );
 		delete_transient( 'global_styles_' . get_stylesheet() );
 		delete_transient( 'gutenberg_global_styles' );
